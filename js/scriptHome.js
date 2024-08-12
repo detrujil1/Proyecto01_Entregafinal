02681b377ff28b2120f62084a1c7eb86c2b764f2
detrujil1 <ingtrujillo6@gmail.com>
2024-08-11 20:54:22 -0500

    add team black

---
+++ js/scriptHome.js
@@ -0,0 +1,11 @@
+const botonLuna = document.querySelector("#botonLuna")
+const teamBlack =document.querySelector("body")
+const mainBlack =document.querySelector("main")
+const recipeContainers = document.querySelectorAll(".imagenReceta");
+
+botonLuna.addEventListener("click", () =>{
+    teamBlack.classList.toggle("bodyBlack")
+    mainBlack.classList.toggle("mainBlack")
+    recipeContainers.classList.toggle("imagenRecetablack")
+})
+
